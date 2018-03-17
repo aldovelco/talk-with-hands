50f3bf2dbab3d58ef0dabebe646c82c788698c51
Velasco <aldo.velasco@accenture.com>
2018-03-17 13:55:39 -0600

    Create search bar, dictionary and signs components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { DictionaryComponent } from './dictionary/dictionary.component';
+import { SignComponent } from './sign/sign.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    DictionaryComponent,
+    SignComponent
   ],
   imports: [
     BrowserModule
